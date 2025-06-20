1bdebcbb20d659a591fce25f02b4001009586767
Subhojit Paul <subhojitpaul.paul10@gmail.com>
2025-06-20 18:31:45 +0530

    fixed cors errors

--- backend/src/sockets.js
+++ backend/src/sockets.js
@@ -2,8 +2,19 @@ import { Server } from 'socket.io';
 import { logger } from './utils/logger.js';
 
 let io;
+
 export function initSockets(server) {
-    io = new Server(server, { cors: { origin: '*' } });
+    io = new Server(server, {
+        cors: {
+            origin: [
+                'https://disaster-response-bgzp8tc3w-subhojit-pauls-projects-0529b500.vercel.app', // Vercel frontend
+                'http://localhost:4001' // local testing
+            ],
+            methods: ['GET', 'POST'],
+            credentials: true
+        }
+    });
+
     io.on('connection', (socket) => {
         logger.info({ action: 'socket_connected', id: socket.id });
     });
